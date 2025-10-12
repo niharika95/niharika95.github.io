eabfa4cdc1c8ff9735ce81a02fb941a224104bc3
Dishant Kaushik <dkaushik95@gmail.com>
2025-10-12 09:45:49 -0700

    feat: implement ScrollToTop component for improved navigation experience

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
 import './index.css';
+
 import { HashRouter, Route, Routes } from 'react-router-dom';
+
+import AdmissionsProcessAcceleration from './screens/AdmissionsProcessAcceleration/AdmissionsProcessAcceleration';
 import Home from './screens/Home/Home';
-import Project from './screens/Project/Project';
-import Resume from './screens/Resume/Resume';
+import InsuranceCompanyWebsiteRedesign from './screens/InsuranceCompanyWebsiteRedesign/InsuranceCompanyWebsiteRedesign';
 import IntelligentCampaignBuilder from './screens/IntelligentCampaignBuilder/IntelligentCampaignBuilder';
 import LoanAppExperienceOptimization from './screens/LoanAppExperienceOptimization/LoanAppExperienceOptimization';
-import AdmissionsProcessAcceleration from './screens/AdmissionsProcessAcceleration/AdmissionsProcessAcceleration';
-import InsuranceCompanyWebsiteRedesign from './screens/InsuranceCompanyWebsiteRedesign/InsuranceCompanyWebsiteRedesign';
 import MainLayout from './components/MainLayout/MainLayout';
+import Project from './screens/Project/Project';
+import React from 'react';
+import Resume from './screens/Resume/Resume';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import { createRoot } from 'react-dom/client';
 import featureFlags from './config/featureFlags';
 
 const container = document.getElementById('root');
@@ -18,6 +21,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
