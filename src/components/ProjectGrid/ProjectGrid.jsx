588c359744f6cbe9ee9cb2bb2960bd3ea5b31d4e
Dishant Kaushik <dkaushik95@gmail.com>
2025-10-12 23:49:18 -0700

    feat: enhance ProjectGrid layout for better mobile responsiveness and visual hierarchy

--- src/components/ProjectGrid/ProjectGrid.jsx
+++ src/components/ProjectGrid/ProjectGrid.jsx
@@ -37,44 +37,47 @@ function ProjectGrid() {
   return (
     <section className='' id='projects'>
       <ContentContainer>
-        <div className='grid grid-cols-2 gap-0 max-lg:grid-cols-1'>
-          {projects.map((project, index) => (
-            <a
-              key={index}
-              href={project.link}
-              className='bg-neutral-100 flex flex-col gap-[10px] h-[500px] overflow-hidden px-[100px] py-[60px] relative no-underline hover:bg-neutral-200 transition-colors max-lg:px-[60px] max-md:px-[40px] max-md:h-[400px]'
-            >
-              <div className='flex flex-col gap-[20px] z-10 max-w-[400px]'>
-                <h3 className='font-playfair font-semibold text-[32px] text-black leading-normal m-0 max-md:text-[28px]'>
-                  {project.title}
-                </h3>
-                <p className='font-mulish font-light text-[20px] text-black leading-[1.5] m-0 max-md:text-[18px]'>
-                  {project.description}
-                </p>
-              </div>
+        <div className='grid grid-cols-2 gap-0'>
+          {projects.map(
+            ({ description, image, link, title, isMobile }, index) => {
+              // Mobile project has horizontal layout
 
-              {/* Project Image */}
-              <div
-                className={`absolute ${
-                  project.isMobile
-                    ? 'h-[615px] left-[calc(50%+150.5px)] -translate-x-1/2 rounded-[36px] top-[134px] w-[275px] max-lg:left-1/2 max-lg:top-[200px]'
-                    : 'h-[480px] left-1/2 top-[266px] -translate-x-1/2 w-[520px] max-lg:w-[400px] max-lg:h-[380px] max-md:w-[300px] max-md:h-[280px]'
-                }`}
-              >
-                <img
-                  alt={project.title}
-                  className={`absolute inset-0 max-w-none object-cover size-full grayscale contrast-125 ${
-                    project.isMobile ? 'rounded-[36px]' : ''
+              return (
+                <a
+                  key={index}
+                  href={link}
+                  className={`bg-neutral-100 px-[20px] hover:bg-amber-50 gap-[40px] overflow-hidden h-[500px] pt-[50px] no-underline flex ${
+                    isMobile
+                      ? 'flex-row '
+                      : 'flex-col justify-between'
                   }`}
-                  src={project.image}
-                  loading='lazy'
-                />
-                {project.isMobile && (
-                  <div className='absolute border-4 border-black border-solid inset-[-4px] rounded-[40px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]' />
-                )}
-              </div>
-            </a>
-          ))}
+                >
+                  {/* Text Content */}
+                  <div
+                    className={`flex flex-col gap-[20px] z-10 ${
+                      isMobile ? 'max-w-[400px]' : 'max-w-[400px]'
+                    }`}
+                  >
+                    <h3 className='font-playfair font-semibold text-[32px] text-black leading-normal m-0'>
+                      {title}
+                    </h3>
+                    <p className='font-mulish font-light text-[20px] text-black leading-[1.5] m-0'>
+                      {description}
+                    </p>
+                  </div>
+
+                  {/* Project Image */}
+
+                  <img
+                    alt={title}
+                    className={`${isMobile ? 'w-[60%] h-auto object-contain object-bottom flex-self-end' : 'w-full'}`}
+                    src={image}
+                    loading='lazy'
+                  />
+                </a>
+              );
+            }
+          )}
         </div>
       </ContentContainer>
     </section>
